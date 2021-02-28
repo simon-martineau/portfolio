f94ffcc1b4998a49d2f631e7150b1cdeb96860b1
Simon Martineau <simon.martineau.3@ulaval.ca>
2021-02-28 15:36:24 -0500

    Skeleton for projects

---
+++ src/components/ProjectList.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import ProjectCard from "./ProjectCard";
+
+export default function ProjectList() {
+  return (
+    <div>
+      <ProjectCard /> {/* There will be many, this is just a skeleton */}
+    </div>
+  );
+}
---
+++ src/components/ProjectSection.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import ProjectList from "./ProjectList";
+import TagList from "./TagList";
+
+export default function ProjectSection() {
+  return (
+    <div>
+      <ProjectList />
+      <TagList />
+    </div>
+  );
+}
